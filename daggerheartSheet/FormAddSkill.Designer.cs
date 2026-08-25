8ebf5a5b381cb085c326d7da18d84a5ddde9d654
Vitorr-AF <vitoraugustofranca7@gmail.com>
2026-08-24 21:52:49 -0300

    Controles do FormAddSkill nomeados

--- daggerheartSheet/FormAddSkill.Designer.cs
+++ daggerheartSheet/FormAddSkill.Designer.cs
@@ -29,27 +29,27 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxSkillName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxSkillType = new System.Windows.Forms.ComboBox();
+            this.comboBoxSkillTrait = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBoxSkillSource = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBoxSkillRange = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBoxSkillDomain = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRecallCost = new System.Windows.Forms.NumericUpDown();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxSkillDamage = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBoxItemDescription = new System.Windows.Forms.TextBox();
+            this.textBoxSkillDescription = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.buttonItemColor = new System.Windows.Forms.Button();
-            this.buttonCancelItem = new System.Windows.Forms.Button();
-            this.buttonSaveItem = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.buttonSkillColor = new System.Windows.Forms.Button();
+            this.buttonCancelSkill = new System.Windows.Forms.Button();
+            this.buttonSaveSkill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRecallCost)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -61,12 +61,12 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome da Habilidade:";
             // 
-            // textBox1
+            // textBoxSkillName
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBoxSkillName.Location = new System.Drawing.Point(13, 30);
+            this.textBoxSkillName.Name = "textBoxSkillName";
+            this.textBoxSkillName.Size = new System.Drawing.Size(130, 20);
+            this.textBoxSkillName.TabIndex = 1;
             // 
             // label2
             // 
@@ -77,21 +77,35 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Tipo:";
             // 
-            // comboBox1
+            // comboBoxSkillType
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(198, 30);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 21);
-            this.comboBox1.TabIndex = 3;
+            this.comboBoxSkillType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSkillType.FormattingEnabled = true;
+            this.comboBoxSkillType.Items.AddRange(new object[] {
+            "",
+            "Habilidade",
+            "Magia"});
+            this.comboBoxSkillType.Location = new System.Drawing.Point(198, 30);
+            this.comboBoxSkillType.Name = "comboBoxSkillType";
+            this.comboBoxSkillType.Size = new System.Drawing.Size(161, 21);
+            this.comboBoxSkillType.TabIndex = 3;
             // 
-            // comboBox2
+            // comboBoxSkillTrait
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(387, 30);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(161, 21);
-            this.comboBox2.TabIndex = 5;
+            this.comboBoxSkillTrait.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSkillTrait.FormattingEnabled = true;
+            this.comboBoxSkillTrait.Items.AddRange(new object[] {
+            "",
+            "Agilidade",
+            "Força",
+            "Finesse",
+            "Instinto",
+            "Presença",
+            "Conhecimento"});
+            this.comboBoxSkillTrait.Location = new System.Drawing.Point(387, 30);
+            this.comboBoxSkillTrait.Name = "comboBoxSkillTrait";
+            this.comboBoxSkillTrait.Size = new System.Drawing.Size(161, 21);
+            this.comboBoxSkillTrait.TabIndex = 5;
             // 
             // label3
             // 
@@ -102,13 +116,19 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Atributo:";
             // 
-            // comboBox3
+            // comboBoxSkillSource
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(198, 85);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(161, 21);
-            this.comboBox3.TabIndex = 7;
+            this.comboBoxSkillSource.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSkillSource.FormattingEnabled = true;
+            this.comboBoxSkillSource.Items.AddRange(new object[] {
+            "Classe/Subclasse",
+            "Carta de Domínio",
+            "Ancestraldade/Comunidade",
+            "Outros"});
+            this.comboBoxSkillSource.Location = new System.Drawing.Point(198, 85);
+            this.comboBoxSkillSource.Name = "comboBoxSkillSource";
+            this.comboBoxSkillSource.Size = new System.Drawing.Size(161, 21);
+            this.comboBoxSkillSource.TabIndex = 7;
             // 
             // label4
             // 
@@ -119,13 +139,21 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Fonte:";
             // 
-            // comboBox4
+            // comboBoxSkillRange
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(387, 85);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(161, 21);
-            this.comboBox4.TabIndex = 9;
+            this.comboBoxSkillRange.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSkillRange.FormattingEnabled = true;
+            this.comboBoxSkillRange.Items.AddRange(new object[] {
+            "",
+            "Corpo-a-corpo",
+            "Muito perto",
+            "Perto",
+            "Distante",
+            "Muito distante"});
+            this.comboBoxSkillRange.Location = new System.Drawing.Point(387, 85);
+            this.comboBoxSkillRange.Name = "comboBoxSkillRange";
+            this.comboBoxSkillRange.Size = new System.Drawing.Size(161, 21);
+            this.comboBoxSkillRange.TabIndex = 9;
             // 
             // label5
             // 
@@ -136,13 +164,14 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "Alcance:";
             // 
-            // comboBox5
+            // comboBoxSkillDomain
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(198, 135);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(161, 21);
-            this.comboBox5.TabIndex = 11;
+            this.comboBoxSkillDomain.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSkillDomain.FormattingEnabled = true;
+            this.comboBoxSkillDomain.Location = new System.Drawing.Point(198, 135);
+            this.comboBoxSkillDomain.Name = "comboBoxSkillDomain";
+            this.comboBoxSkillDomain.Size = new System.Drawing.Size(161, 21);
+            this.comboBoxSkillDomain.TabIndex = 11;
             // 
             // label6
             // 
@@ -153,12 +182,17 @@
             this.label6.TabIndex = 10;
             this.label6.Text = "Domínio:";
             // 
-            // numericUpDown1
+            // numericUpDownRecallCost
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(390, 191);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDownRecallCost.Location = new System.Drawing.Point(390, 191);
+            this.numericUpDownRecallCost.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownRecallCost.Name = "numericUpDownRecallCost";
+            this.numericUpDownRecallCost.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownRecallCost.TabIndex = 12;
             // 
             // label7
             // 
@@ -169,12 +203,12 @@
             this.label7.TabIndex = 13;
             this.label7.Text = "Custo de Troca:";
             // 
-            // textBox2
+            // textBoxSkillDamage
             // 
-            this.textBox2.Location = new System.Drawing.Point(387, 136);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(161, 20);
-            this.textBox2.TabIndex = 15;
+            this.textBoxSkillDamage.Location = new System.Drawing.Point(387, 136);
+            this.textBoxSkillDamage.Name = "textBoxSkillDamage";
+            this.textBoxSkillDamage.Size = new System.Drawing.Size(161, 20);
+            this.textBoxSkillDamage.TabIndex = 15;
             // 
             // label8
             // 
@@ -185,13 +219,13 @@
             this.label8.TabIndex = 14;
             this.label8.Text = "Dano:";
             // 
-            // textBoxItemDescription
+            // textBoxSkillDescription
             // 
-            this.textBoxItemDescription.Location = new System.Drawing.Point(12, 190);
-            this.textBoxItemDescription.Multiline = true;
-            this.textBoxItemDescription.Name = "textBoxItemDescription";
-            this.textBoxItemDescription.Size = new System.Drawing.Size(369, 153);
-            this.textBoxItemDescription.TabIndex = 16;
+            this.textBoxSkillDescription.Location = new System.Drawing.Point(12, 190);
+            this.textBoxSkillDescription.Multiline = true;
+            this.textBoxSkillDescription.Name = "textBoxSkillDescription";
+            this.textBoxSkillDescription.Size = new System.Drawing.Size(369, 153);
+            this.textBoxSkillDescription.TabIndex = 16;
             // 
             // label9
             // 
@@ -202,62 +236,62 @@
             this.label9.TabIndex = 17;
             this.label9.Text = "Descrição:";
             // 
-            // buttonItemColor
+            // buttonSkillColor
             // 
-            this.buttonItemColor.Location = new System.Drawing.Point(415, 244);
-            this.buttonItemColor.Name = "buttonItemColor";
-            this.buttonItemColor.Size = new System.Drawing.Size(116, 99);
-            this.buttonItemColor.TabIndex = 18;
-            this.buttonItemColor.Text = "Escolher Cor";
-            this.buttonItemColor.UseVisualStyleBackColor = true;
+            this.buttonSkillColor.Location = new System.Drawing.Point(413, 226);
+            this.buttonSkillColor.Name = "buttonSkillColor";
+            this.buttonSkillColor.Size = new System.Drawing.Size(116, 99);
+            this.buttonSkillColor.TabIndex = 18;
+            this.buttonSkillColor.Text = "Escolher Cor";
+            this.buttonSkillColor.UseVisualStyleBackColor = true;
             // 
-            // buttonCancelItem
+            // buttonCancelSkill
             // 
-            this.buttonCancelItem.Location = new System.Drawing.Point(16, 349);
-            this.buttonCancelItem.Name = "buttonCancelItem";
-            this.buttonCancelItem.Size = new System.Drawing.Size(75, 23);
-            this.buttonCancelItem.TabIndex = 19;
-            this.buttonCancelItem.Text = "Cancelar";
-            this.buttonCancelItem.UseVisualStyleBackColor = true;
+            this.buttonCancelSkill.Location = new System.Drawing.Point(16, 349);
+            this.buttonCancelSkill.Name = "buttonCancelSkill";
+            this.buttonCancelSkill.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelSkill.TabIndex = 19;
+            this.buttonCancelSkill.Text = "Cancelar";
+            this.buttonCancelSkill.UseVisualStyleBackColor = true;
             // 
-            // buttonSaveItem
+            // buttonSaveSkill
             // 
-            this.buttonSaveItem.Location = new System.Drawing.Point(473, 349);
-            this.buttonSaveItem.Name = "buttonSaveItem";
-            this.buttonSaveItem.Size = new System.Drawing.Size(75, 23);
-            this.buttonSaveItem.TabIndex = 20;
-            this.buttonSaveItem.Text = "Salvar";
-            this.buttonSaveItem.UseVisualStyleBackColor = true;
+            this.buttonSaveSkill.Location = new System.Drawing.Point(473, 349);
+            this.buttonSaveSkill.Name = "buttonSaveSkill";
+            this.buttonSaveSkill.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveSkill.TabIndex = 20;
+            this.buttonSaveSkill.Text = "Salvar";
+            this.buttonSaveSkill.UseVisualStyleBackColor = true;
             // 
             // FormAddSkill
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 387);
-            this.Controls.Add(this.buttonSaveItem);
-            this.Controls.Add(this.buttonCancelItem);
-            this.Controls.Add(this.buttonItemColor);
+            this.Controls.Add(this.buttonSaveSkill);
+            this.Controls.Add(this.buttonCancelSkill);
+            this.Controls.Add(this.buttonSkillColor);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.textBoxItemDescription);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxSkillDescription);
+            this.Controls.Add(this.textBoxSkillDamage);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.numericUpDownRecallCost);
+            this.Controls.Add(this.comboBoxSkillDomain);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBoxSkillRange);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBoxSkillSource);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBoxSkillTrait);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBoxSkillType);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxSkillName);
             this.Controls.Add(this.label1);
             this.Name = "FormAddSkill";
-            this.Text = "FormAddSkill";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.Text = "Adicionar Habilidade";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRecallCost)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -266,25 +300,25 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxSkillName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxSkillType;
+        private System.Windows.Forms.ComboBox comboBoxSkillTrait;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBoxSkillSource;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBoxSkillRange;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBoxSkillDomain;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDownRecallCost;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxSkillDamage;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBoxItemDescription;
+        private System.Windows.Forms.TextBox textBoxSkillDescription;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Button buttonItemColor;
-        private System.Windows.Forms.Button buttonCancelItem;
-        private System.Windows.Forms.Button buttonSaveItem;
+        private System.Windows.Forms.Button buttonSkillColor;
+        private System.Windows.Forms.Button buttonCancelSkill;
+        private System.Windows.Forms.Button buttonSaveSkill;
     }
 }
